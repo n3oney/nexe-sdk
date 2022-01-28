415379cebf2031a8d9ba3eb91ebbe13deb699200
neoney <neoney@pm.me>
2022-01-28 02:12:22 +0100

    feat: add author setting

--- src/memberEmbed.ts
+++ src/memberEmbed.ts
@@ -1,5 +1,17 @@
 import { GuildMember, MessageEmbed } from 'discord.js';
 
-export function memberEmbed(member: GuildMember) {
-  return new MessageEmbed().setColor(member.roles.highest.color);
+export function memberEmbed(member: GuildMember, setAuthor = true) {
+  const embed = new MessageEmbed()
+    .setColor(member.roles.highest.color);
+  if (setAuthor) {
+    embed.setAuthor({
+      iconURL: member.displayAvatarURL({
+        dynamic: true,
+        size: 128
+      }),
+      name: member.displayName
+    });
+  }
+
+  return embed;
 }
